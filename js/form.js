ece506e64f0019b2771b2dbf5d2436277bf06560
Zhirova <42269523+Zhirova@users.noreply.github.com>
2020-03-18 11:56:37 +0700

    исправления формы

--- js/form.js
+++ js/form.js
@@ -1,21 +1,43 @@
 var link = document.querySelector(".brown-button-big");
 var form = document.querySelector(".search-form");
 
-var checkin = form.querySelector("[name=arrival-date]");
-var checkout = form.querySelector("[name=departure-date]");
+var checkIn = form.querySelector("[name=arrival-date]");
+var checkOut = form.querySelector("[name=departure-date]");
 var adult = form.querySelector("[name=adult]");
 var child = form.querySelector("[name=child]");
 
-link.addEventListener("click", function (evt) {          //Скрытие формы
+var isStorageSupport = true;
+var storageAdult = "";
+var storageChild = "";
+
+try {
+  storageAdult = localStorage.getItem("adult");
+  storageChild = localStorage.getItem("child");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+form.classList.add("form-hide");                         //Скрытие формы
+
+link.addEventListener("click", function (evt) {          //Переключение класса
+  evt.preventDefault();
   form.classList.toggle("form-hide");
-})
+  if (storageAdult || storageChild) {
+    child.value = storageChild;
+    adult.value = storageAdult;
+  }
+});
 
 form.addEventListener("submit", function (evt) {         //отправка формы
-  if (!checkin.value || !checkout.value || !adult.value || !child.value) {
-  evt.preventDefault();
-  console.log("Заполните все поля!");
+  if (!checkIn.value || !checkOut.value || !adult.value || !child.value) {
+    evt.preventDefault();
+    form.classList.remove("form-error");
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("form-error");
   } else {
+      if (isStorageSupport) {
       localStorage.setItem("adult", adult.value);            //взрослые и дети в  localStorage
       localStorage.setItem("child", child.value);
+      }
     }
 });
